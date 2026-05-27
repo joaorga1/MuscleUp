76f3534f59ee9c084ba99c7d435a2fe78eda93ef
João Alves <joaorga1@gmail.com>
2026-05-27 09:12:47 +0100

    fix imports

--- app/src/main/java/pt/ipt/dama/muscleup/ui/navigation/AppNavigation.kt
+++ app/src/main/java/pt/ipt/dama/muscleup/ui/navigation/AppNavigation.kt
@@ -2,21 +2,25 @@ package pt.ipt.dama.muscleup.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import pt.ipt.dama.muscleup.ui.screens.ExerciseScreen
-import pt.ipt.dama.muscleup.ui.screens.HomeScreen
-import pt.ipt.dama.muscleup.ui.screens.LoginScreen
-import pt.ipt.dama.muscleup.ui.screens.ProfileScreen
-import pt.ipt.dama.muscleup.ui.screens.RegisterScreen
-import pt.ipt.dama.muscleup.ui.screens.WorkoutScreen
+import androidx.navigation.navArgument
+import pt.ipt.dama.muscleup.ui.screens.auth.LoginScreen
+import pt.ipt.dama.muscleup.ui.screens.auth.RegisterScreen
+import pt.ipt.dama.muscleup.ui.screens.exercise.ExerciseScreen
+import pt.ipt.dama.muscleup.ui.screens.home.HomeScreen
+import pt.ipt.dama.muscleup.ui.screens.profile.ProfileScreen
+import pt.ipt.dama.muscleup.ui.screens.workout.WorkoutScreen
 
 sealed class Screen(val route: String) {
     object Login    : Screen("login")
     object Register : Screen("register")
     object Home     : Screen("home")
-    object Workout  : Screen("workout")
+    object Workout  : Screen("workout/{workoutId}") {
+        fun go(workoutId: String) = "workout/$workoutId"
+    }
     object Exercise : Screen("exercise")
     object Profile  : Screen("profile")
 }
@@ -33,7 +37,13 @@ fun AppNavigation(
         composable(Screen.Login.route)    { LoginScreen(navController) }
         composable(Screen.Register.route) { RegisterScreen(navController) }
         composable(Screen.Home.route)     { HomeScreen(navController) }
-        composable(Screen.Workout.route)  { WorkoutScreen() }
+        composable(
+            route = Screen.Workout.route,
+            arguments = listOf(navArgument("workoutId") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val workoutId = backStackEntry.arguments?.getString("workoutId") ?: ""
+            WorkoutScreen(navController = navController, workoutId = workoutId)
+        }
         composable(Screen.Exercise.route) { ExerciseScreen() }
         composable(Screen.Profile.route)  { ProfileScreen(navController) }
     }
